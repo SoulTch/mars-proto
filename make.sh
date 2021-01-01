10518ea8ef5a5d0d7d802c25c5e287bc7aec005b
jerron <jerron@watcha.com>
2021-01-01 14:04:24 +0900

    First commit

---
+++ make.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e -o pipefail
+trap '[ "$?" -eq 0 ] || echo "Error Line:<$LINENO> Error Function:<${FUNCNAME}>"' EXIT
+cd `dirname $0`
+CURRENT=`pwd`
+
+function makedir {
+    mkdir -p lib/mars-proto-go
+    mkdir -p lib/mars-proto-js
+}
+
+function build {
+    makedir
+    protoc --go_out=lib/mars-proto-go --js_out=lib/mars-proto-js mars.proto
+}
+
+CMD=$1
+shift
+$CMD $*
